c27e1bd4d366d115bca11f8a0abf676275b3fdf3
thiagovra <47828800+thiagovra@users.noreply.github.com>
2021-12-01 12:11:37 -0300

    Tasks 2 e 3 do teste

---
+++ CCAtest2/CCA2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class QuadraticEquation
+{
+    public static Tuple<double, double> FindRoots(double a, double b, double c)
+    {
+        var delta = b * b - 4 * a * c;
+        if (delta < 0)
+        {
+            return null;
+        }
+        else
+        {
+            var pos = 1.0;
+            var neg = -1.0;
+            var result1 = (pos * Math.Sqrt(delta) - b) / (2.0 * a);
+            var result2 = (neg * Math.Sqrt(delta) - b) / (2.0 * a);
+            return new Tuple<double, double>(result1, result2);
+        };
+    }
+
+    public static void Main(string[] args)
+    {
+        Tuple<double, double> roots = QuadraticEquation.FindRoots(2, 10, 8);
+        Console.WriteLine("Roots: " + roots.Item1 + ", " + roots.Item2);
+    }
+}
---
+++ CCAtest3/CCA3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class SortedSearch
+{
+    public static int CountNumbers(int[] sortedArray, int lessThan)
+    {
+        int lengthOfArray = sortedArray.Length;
+        //Array sem elementos
+        if (lengthOfArray == 0) {
+            return 0;
+        }
+        
+        //Menor valor maior que o minimo permitido
+        if (sortedArray[0] >= lessThan) { 
+            return 0; 
+        }
+
+        
+        if (sortedArray[lengthOfArray - 1] < lessThan)
+        {
+            return lengthOfArray;
+        }
+
+        int index = Array.BinarySearch(sortedArray, lessThan);
+
+        if (index < 0){
+            return ~index;
+        }
+
+        return index;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(SortedSearch.CountNumbers(new int[] { 1, 3, 5, 7 }, 4));
+    }
+}
